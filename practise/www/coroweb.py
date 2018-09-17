e72e488876cde760ffecf527cf0a0526f40230dd
yhhu <yhhu@thoughtworks.com>
2018-09-17 10:17:38 +0800

    feat: add practise apis.py

--- practise/www/coroweb.py
+++ practise/www/coroweb.py
@@ -6,7 +6,7 @@ __author__ = 'Ryn'
 import asyncio, os, inspect, logging, functools
 from urllib import parse
 from aiohttp import web
-# from apis import APIError
+from apis import APIError
 
 def get(path):
   '''
